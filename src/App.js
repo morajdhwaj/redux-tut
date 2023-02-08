d875a11e42f538a76509db7a05e654db57835d89
moraj <dhwajmoraj7@gmail.com>
2023-02-08 13:17:49 +0530

    Applying css n home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import HomeContainer from "./container/HomeContainer";
+import "./App.css"
+// import User from "./User";
 function App() {
   return (
-    <div>
+    <div className="App ">
+      <h1>Home components</h1>
       {/* <User data={{name:"moraj", age:27}} />  */}
       <HomeContainer />
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,25 @@
-// import './App.css';
-
+import "../App.css";
 
 const Home = () => {
   return (
-    <div className="bg">
-      <h1>Home</h1>
+    <div className="">
+      <div className="add-to-cart">
+        <img
+          className="cartimage"
+          src="https://static.vecteezy.com/system/resources/thumbnails/004/798/846/small/shopping-cart-logo-or-icon-design-vector.jpg"
+        />
+      </div>
+
       <div className="cart-wrapper">
         <div className="img-wrapper item">
           <img src="https://m.media-amazon.com/images/I/51jweLlbeTL._SL1024_.jpg" />
         </div>
         <div className="text-wrapper item">
-        <span>oppo</span>
-        <span>Price- 15000</span>
+          <span>oppo</span>
+          <span>Price- 15000</span>
         </div>
         <div className="btn-wrapper item bg">
-            <button className="btn-wrapper bg">Add To Cart</button>
+          <button className="btn-wrapper bg">Add To Cart</button>
         </div>
       </div>
     </div>
